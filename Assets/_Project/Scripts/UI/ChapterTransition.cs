c5a0c37a9d1d2e148e952dcdcbed4d6e63cda8e7
truongvu009-ae <truongvu2k6sn@gmail.com>
2026-03-26 00:26:25 +0700

    test

--- Assets/_Project/Scripts/UI/ChapterTransition.cs
+++ Assets/_Project/Scripts/UI/ChapterTransition.cs
@@ -1,13 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Events;
 
 public class ChapterTransition : MonoBehaviour
 {
     [SerializeField] private bool _isPlaying = false;
+    [SerializeField] private float _duration = 2f;
+    [SerializeField] private string _chapterName;
+    public UnityEvent OnTransitionComplete = new UnityEvent();
+
     public void PlayTransition(string chapterName, string year)
     {
+        if (_isPlaying) return;
         _isPlaying = true;
+        _chapterName = chapterName;
         StartCoroutine(Run());
     }
-    private IEnumerator Run() { yield return new WaitForSeconds(2f); _isPlaying = false; }
+    private IEnumerator Run() 
+    { 
+        yield return new WaitForSeconds(_duration); 
+        _isPlaying = false; 
+        OnTransitionComplete.Invoke();
+    }
 }
