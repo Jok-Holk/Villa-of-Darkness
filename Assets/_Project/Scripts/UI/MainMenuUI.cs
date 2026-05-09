f7a3f87d8683d8e09ca61377044fb9c07f602d03
truongvu009-ae <truongvu2k6sn@gmail.com>
2026-05-09 15:29:19 +0700

    feat(phase2):complete menuui and pauseui

--- Assets/_Project/Scripts/UI/MainMenuUI.cs
+++ Assets/_Project/Scripts/UI/MainMenuUI.cs
@@ -6,7 +6,9 @@ public class MainMenuUI : MonoBehaviour
 {
     public UnityEvent OnMenuOpen = new UnityEvent();
 
-    public void StartGame() { GameManager.Instance?.LoadChapter(1); }
+    public void StartGame() { GameManager.Instance?.LoadChapter(1);
+        SceneManager.LoadScene("TestMenu"); // đổi tên đúng scene của bạn
+    }
     public void QuitGame()  { Application.Quit(); }
     public void Show() { gameObject.SetActive(true); OnMenuOpen.Invoke(); }
     public void Hide() { gameObject.SetActive(false); }
